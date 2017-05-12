07ffd80d43059e2103361c7a92d129cf8d2dbc05
chenzhiqiang1 <1670347547@qq.com>
2017-05-12 12:00:41 +0800

    字符串空格替换

--- 九度oj/1510.cpp
+++ 九度oj/1510.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+//牛客网上通过，但是vs上数组扩容问题为解决
+class Solution {
+public:
+    void replaceSpace(char *str,int length) {
+        int i,j,len;
+        int count=0;
+        len=strlen(str);
+        if(len==NULL)
+            return;
+        for(i=0;i<=len;i++){
+            if(str[i]==' ')
+                count++;
+        }
+        int newlen=len+2*count+1;
+        for(j=len+1;j>=0;j--){
+            if(str[j]==' '){
+                str[newlen--]='0';
+                str[newlen--]='2';
+                str[newlen--]='\%';
+            }
+            else
+                str[newlen--]=str[j];
+             
+        }
+             
+    }
+};
+
+
 int main(){
 	int i=0;
 	string a;
